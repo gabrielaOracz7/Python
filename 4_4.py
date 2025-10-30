37dfa4fe3ff6da59fc9b3ad1ea57f3e499ae63ca
gabrielaOracz7 <gabrielaoracz7@gmail.com>
2025-10-30 13:11:07 +0100

    initial commit

---
+++ 4_4.py
@@ -0,0 +1,21 @@
+# ZADANIE 4.4------------------------------------------------------------
+
+def fibonacci(n):
+    if  not isinstance(n, int) or n<0:
+        raise ValueError('Argument must be non-negative integer.')
+    
+    if n==0 or n==1:
+        return n
+        
+    a = 0
+    b = 1
+    for i in range(2, n+1):
+        a, b=b, a+b
+    return b
+
+
+
+assert fibonacci(2)== 1
+assert fibonacci(0)== 0
+assert fibonacci(6)== 8
+assert fibonacci(10)== 55
