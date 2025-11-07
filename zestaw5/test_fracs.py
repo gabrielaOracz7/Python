93a2b952ad30f1071d0bf1ddda6d9c514ac9141e
gabrielaOracz7 <gabrielaoracz7@gmail.com>
2025-11-07 17:14:07 +0100

    Update test_fracs.py

--- zestaw5/test_fracs.py
+++ zestaw5/test_fracs.py
@@ -14,17 +14,17 @@ class TestFractions(unittest.TestCase):
 
     def test_sub_frac(self):
         self.assertEqual(fracs.sub_frac([1, 2], [4, 8]), self.zero)
-        self.assertEqual(fracs.sub_frac([2, 4], [2,3]),[-1, 6])
+        self.assertEqual(fracs.sub_frac([2, 4], [2,3]), [-1, 6])
         self.assertEqual(fracs.sub_frac([7, 8], [1, 5]), [27, 40])
 
     def test_mul_frac(self):
         self.assertEqual(fracs.mul_frac([1, 2], [4, 8]), [1,4])
-        self.assertEqual(fracs.mul_frac([2, 4], [2,-3]),[-1, 3])
+        self.assertEqual(fracs.mul_frac([2, 4], [2,-3]), [-1, 3])
         self.assertEqual(fracs.mul_frac([0, 8], [1, 5]), self.zero)    
 
     def test_div_frac(self):
-        self.assertEqual(fracs.div_frac([5,6], [1,3]),[5,2] )
-        self.assertEqual(fracs.div_frac([-1, 3], [2, -5]),[5, 6])
+        self.assertEqual(fracs.div_frac([5,6], [1,3]), [5,2] )
+        self.assertEqual(fracs.div_frac([-1, 3], [2, -5]), [5, 6])
         self.assertEqual(fracs.div_frac([0,7], [-1, 2]), self.zero)
         self.assertRaises(ZeroDivisionError, fracs.div_frac, [8,7], [0,5])
 
@@ -47,9 +47,9 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(fracs.cmp_frac([1,-2], [1, 3]), -1)
 
     def test_frac2float(self):
-        self.assertAlmostEqual(fracs.frac2float([1, -2]), -0.50 , 2)
+        self.assertAlmostEqual(fracs.frac2float([4, -6]), -0.67 , 2)
         self.assertAlmostEqual(fracs.frac2float([12, 36]), 0.33, 2)
-        self.assertAlmostEqual(fracs.frac2float([-12, -32]), 0.375, 3)
+        self.assertAlmostEqual(fracs.frac2float([-13, -17]), 0.76, 2)
 
     def tearDown(self):
         pass
